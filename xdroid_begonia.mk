a47faf87ee18252bf22cc9d537228819e2b4b5ae
ios7jbpro <bariskirmizi.208@gmail.com>
2022-10-31 12:25:06 +0300

    Initial Configuration

---
+++ xdroid_begonia.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := begonia
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from begonia device
+$(call inherit-product, device/xiaomi/begonia/device.mk)
+
+# Inherit some common xdroid stuff
+$(call inherit-product, vendor/xdroid/config/common_full_phone.mk)
+
+# Inherit some extras stuff
+$(call inherit-product-if-exists, vendor/extras/extras.mk)
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+$(call inherit-product, packages/apps/ANXCamera/config.mk)
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# xdroid flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+XDROID_BOOT := 1080
+XDROID_MAINTAINER := isot7jbpro
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := begonia
+PRODUCT_NAME := xdroid_begonia
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 8 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Redmi/begonia/begonia:11/RP1A.200720.011/V12.5.8.0.RGGMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="begonia-user 11 RP1A.200720.011 V12.5.8.0.RGGMIXM release-keys" \
+    PRODUCT_NAME="begonia" \
+    TARGET_DEVICE="begonia"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
